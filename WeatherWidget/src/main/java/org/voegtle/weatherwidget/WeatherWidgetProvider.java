4b655e80c805f2db258b89396c9138afba8319a3
christian@voegtle.org <christian@voegtle.org>
2014-08-25 22:38:11 +0200

    Version 1.1 mit flexiblerem Update-Intervall im Widget

--- WeatherWidget/src/main/java/org/voegtle/weatherwidget/WeatherWidgetProvider.java
+++ WeatherWidget/src/main/java/org/voegtle/weatherwidget/WeatherWidgetProvider.java
@@ -1,107 +1,109 @@
 package org.voegtle.weatherwidget;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.preference.PreferenceManager;
-import android.view.View;
 import android.widget.RemoteViews;
 import org.voegtle.weatherwidget.location.LocationFactory;
 import org.voegtle.weatherwidget.location.WeatherLocation;
 import org.voegtle.weatherwidget.preferences.WeatherSettingsReader;
-import org.voegtle.weatherwidget.util.WeatherWidgetUpdater;
 
 import java.util.List;
 
 public class WeatherWidgetProvider extends AppWidgetProvider implements SharedPreferences.OnSharedPreferenceChangeListener {
+  private PendingIntent refreshService;
+  private List<WeatherLocation> locations;
   private Resources res;
   private RemoteViews remoteViews;
-  private List<WeatherLocation> locations;
-
+  private AlarmManager alarmManager;
+  private int intervall = -1;
 
   public WeatherWidgetProvider() {
-
-  }
-
-  @Override
-  public void onEnabled(Context context) {
-    ensureResources(context);
-    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-    setupUserInterface(preferences);
-  }
-
-  private void ensureResources(Context context) {
-    if (res == null) {
-      res = context.getResources();
-      remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_weather);
-      SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-      preferences.registerOnSharedPreferenceChangeListener(this);
-
-      locations = LocationFactory.buildWeatherLocations(res);
-    }
   }
 
   @Override
   public void onUpdate(Context context, final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
     ensureResources(context);
-    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-    setupUserInterface(preferences);
 
-    ComponentName thisWidget = new ComponentName(context, WeatherWidgetProvider.class);
-    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
-    for (int widgetId : allWidgetIds) {
-
-      new WeatherWidgetUpdater(context, appWidgetManager, widgetId, remoteViews, locations)
-          .startWeatherScheduler();
+    for (int widgetId : appWidgetIds) {
 
       PendingIntent pendingOpenApp = createOpenAppIntent(context);
       for (WeatherLocation location : locations) {
         remoteViews.setOnClickPendingIntent(location.getWeatherViewId(), pendingOpenApp);
       }
 
-      PendingIntent pendingRefresh = createRefreshIntent(context, appWidgetIds);
-      remoteViews.setOnClickPendingIntent(R.id.refresh_button, pendingRefresh);
+      remoteViews.setOnClickPendingIntent(R.id.refresh_button, refreshService);
 
       appWidgetManager.updateAppWidget(widgetId, remoteViews);
     }
 
-    super.onUpdate(context, appWidgetManager, appWidgetIds);
+    restartService();
   }
+
+  private void ensureResources(Context context) {
+    if (res == null) {
+      res = context.getResources();
+      alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+      final Intent refreshIntent = new Intent(context, WidgetRefreshService.class);
+      refreshService = PendingIntent.getService(context, 0, refreshIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+      remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_weather);
+      locations = LocationFactory.buildWeatherLocations(res);
+
+      SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+      preferences.registerOnSharedPreferenceChangeListener(this);
+      processPreferences(preferences);
+    }
+  }
+
+  @Override
+  public void onDisabled(Context context) {
+    cancelAlarmService();
+  }
+
+  private void restartService() {
+    cancelAlarmService();
+    runServiceOnce();
+
+    if (intervall > 0) {
+      alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), intervall * 60 * 1000, refreshService);
+    }
+  }
+
+  private void runServiceOnce() {
+    alarmManager.set(AlarmManager.RTC, System.currentTimeMillis() + 10, refreshService);
+  }
+
+  private void cancelAlarmService() {
+    if (refreshService != null) {
+      alarmManager.cancel(refreshService);
+    }
+  }
+
 
   private PendingIntent createOpenAppIntent(Context context) {
     Intent intentOpenApp = new Intent(context, WeatherActivity.class);
     return PendingIntent.getActivity(context, 0, intentOpenApp, PendingIntent.FLAG_UPDATE_CURRENT);
   }
 
-  private PendingIntent createRefreshIntent(Context context, int[] appWidgetIds) {
-    Intent intentRefresh = new Intent(context, WeatherWidgetProvider.class);
-    intentRefresh.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-    intentRefresh.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
-    return PendingIntent.getBroadcast(context, 0, intentRefresh, PendingIntent.FLAG_UPDATE_CURRENT);
-  }
-
   @Override
   public void onSharedPreferenceChanged(SharedPreferences preferences, String s) {
-    setupUserInterface(preferences);
+    processPreferences(preferences);
+    restartService();
   }
 
-  private void setupUserInterface(SharedPreferences preferences) {
+  private void processPreferences(SharedPreferences preferences) {
     WeatherSettingsReader weatherSettingsReader = new WeatherSettingsReader();
     weatherSettingsReader.read(preferences, locations);
 
-    for (WeatherLocation location : locations) {
-      boolean show = location.getPreferences().isShowInWidget();
-      updateVisibility(location.getWeatherViewId(), show);
-    }
-  }
-
-  private void updateVisibility(int id, boolean isVisible) {
-    remoteViews.setViewVisibility(id, isVisible ? View.VISIBLE : View.GONE);
+    intervall = weatherSettingsReader.readIntervall(preferences);
   }
 
 
---
+++ WeatherWidget/src/main/java/org/voegtle/weatherwidget/WidgetRefreshService.java
@@ -0,0 +1,90 @@
+package org.voegtle.weatherwidget;
+
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.RemoteViews;
+import org.voegtle.weatherwidget.location.LocationFactory;
+import org.voegtle.weatherwidget.location.WeatherLocation;
+import org.voegtle.weatherwidget.preferences.WeatherSettingsReader;
+import org.voegtle.weatherwidget.util.WeatherWidgetUpdater;
+
+import java.util.List;
+
+public class WidgetRefreshService extends Service implements SharedPreferences.OnSharedPreferenceChangeListener {
+  private Resources res;
+  private RemoteViews remoteViews;
+  private List<WeatherLocation> locations;
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    ensureResources();
+
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    preferences.registerOnSharedPreferenceChangeListener(this);
+
+    processPreferences(preferences);
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    updateWidget();
+    return super.onStartCommand(intent, flags, startId);
+  }
+
+  private void ensureResources() {
+    if (res == null) {
+      res = getResources();
+      remoteViews = new RemoteViews(getPackageName(), R.layout.widget_weather);
+      locations = LocationFactory.buildWeatherLocations(res);
+    }
+  }
+
+
+  private void updateWidget() {
+    ensureResources();
+
+    ComponentName thisWidget = new ComponentName(this, WeatherWidgetProvider.class);
+    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+    for (int widgetId : allWidgetIds) {
+
+      new WeatherWidgetUpdater(getApplicationContext(), appWidgetManager, widgetId, remoteViews, locations)
+          .startWeatherUpdateThread();
+    }
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences preferences, String s) {
+    processPreferences(preferences);
+    updateWidget();
+  }
+
+  private void processPreferences(SharedPreferences preferences) {
+    WeatherSettingsReader weatherSettingsReader = new WeatherSettingsReader();
+    weatherSettingsReader.read(preferences, locations);
+
+    for (WeatherLocation location : locations) {
+      boolean show = location.getPreferences().isShowInWidget();
+      updateVisibility(location.getWeatherViewId(), show);
+    }
+  }
+
+  private void updateVisibility(int id, boolean isVisible) {
+    remoteViews.setViewVisibility(id, isVisible ? View.VISIBLE : View.GONE);
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return null;
+  }
+
+
+}
