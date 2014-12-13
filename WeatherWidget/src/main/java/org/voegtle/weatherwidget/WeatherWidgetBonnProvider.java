ba6dc78880c1869dae5d9e50d300e6179c7698ee
christian@voegtle.org <christian@voegtle.org>
2014-12-13 20:44:28 +0100

    Projekt umbenannt

--- WeatherWidget/src/main/java/org/voegtle/weatherwidget/WeatherWidgetBonnProvider.java
+++
@@ -1,7 +0,0 @@
-package org.voegtle.weatherwidget;
-
-public class WeatherWidgetBonnProvider extends WeatherWidgetSmallProvider{
-  public WeatherWidgetBonnProvider() {
-    super("http://forstwetter.appspot.com", R.string.city_bonn);
-  }
-}
--- WeatherWidget/src/main/java/org/voegtle/weatherwidget/WeatherWidgetPaderbornProvider.java
+++
@@ -1,8 +0,0 @@
-package org.voegtle.weatherwidget;
-
-public class WeatherWidgetPaderbornProvider extends WeatherWidgetSmallProvider {
-  public WeatherWidgetPaderbornProvider() {
-    super("http://tegelwetter.appspot.com", R.string.city_paderborn);
-  }
-
-}
