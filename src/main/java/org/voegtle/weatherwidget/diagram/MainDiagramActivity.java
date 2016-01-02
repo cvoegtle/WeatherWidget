a0f9083c0f59cef640c9980fbb4008611a73b57f
christian@voegtle.org <christian@voegtle.org>
2016-01-02 19:38:09 +0100

    Wintertage vorziehen

--- src/main/java/org/voegtle/weatherwidget/diagram/MainDiagramActivity.java
+++ src/main/java/org/voegtle/weatherwidget/diagram/MainDiagramActivity.java
@@ -15,8 +15,8 @@ public class MainDiagramActivity extends DiagramActivity {
     addDiagram(DiagramEnum.temperature7days);
     addDiagram(DiagramEnum.pb_bali_leo);
     addDiagram(DiagramEnum.average7days);
-    addDiagram(DiagramEnum.summerdays);
     addDiagram(DiagramEnum.winterdays);
+    addDiagram(DiagramEnum.summerdays);
   }
 
 
@@ -42,11 +42,11 @@ public class MainDiagramActivity extends DiagramActivity {
         viewPager.setCurrentItem(2, true);
         return true;
 
-      case R.id.action_summerdays:
+      case R.id.action_winterdays:
         viewPager.setCurrentItem(3, true);
         return true;
 
-      case R.id.action_winterdays:
+      case R.id.action_summerdays:
         viewPager.setCurrentItem(4, true);
         return true;
 
