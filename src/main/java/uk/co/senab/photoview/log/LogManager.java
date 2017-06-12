2316d652b164adf0955f25142266209ec93a54f1
christian@voegtle.org <christian@voegtle.org>
2017-06-12 21:38:45 +0200

    PhotoView aufräumen vor Kotlin-Port

--- src/main/java/uk/co/senab/photoview/log/LogManager.java
+++ src/main/java/uk/co/senab/photoview/log/LogManager.java
@@ -24,10 +24,6 @@ public final class LogManager {
 
     private static Logger logger = new LoggerDefault();
 
-    public static void setLogger(Logger newLogger) {
-        logger = newLogger;
-    }
-
     public static Logger getLogger() {
         return logger;
     }
--- src/main/java/uk/co/senab/photoview/log/Logger.java
+++ src/main/java/uk/co/senab/photoview/log/Logger.java
@@ -20,25 +20,6 @@ package uk.co.senab.photoview.log;
  */
 public interface Logger {
     /**
-     * Send a {@link android.util.Log#VERBOSE} log message.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     */
-    int v(String tag, String msg);
-
-    /**
-     * Send a {@link android.util.Log#VERBOSE} log message and log the exception.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     * @param tr  An exception to log
-     */
-    int v(String tag, String msg, Throwable tr);
-
-    /**
      * Send a {@link android.util.Log#DEBUG} log message.
      *
      * @param tag Used to identify the source of a log message.  It usually identifies
@@ -48,16 +29,6 @@ public interface Logger {
     int d(String tag, String msg);
 
     /**
-     * Send a {@link android.util.Log#DEBUG} log message and log the exception.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     * @param tr  An exception to log
-     */
-    int d(String tag, String msg, Throwable tr);
-
-    /**
      * Send an {@link android.util.Log#INFO} log message.
      *
      * @param tag Used to identify the source of a log message.  It usually identifies
@@ -65,35 +36,6 @@ public interface Logger {
      * @param msg The message you would like logged.
      */
     int i(String tag, String msg);
-
-    /**
-     * Send a {@link android.util.Log#INFO} log message and log the exception.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     * @param tr  An exception to log
-     */
-    int i(String tag, String msg, Throwable tr);
-
-    /**
-     * Send a {@link android.util.Log#WARN} log message.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     */
-    int w(String tag, String msg);
-
-    /**
-     * Send a {@link android.util.Log#WARN} log message and log the exception.
-     *
-     * @param tag Used to identify the source of a log message.  It usually identifies
-     *            the class or activity where the log call occurs.
-     * @param msg The message you would like logged.
-     * @param tr  An exception to log
-     */
-    int w(String tag, String msg, Throwable tr);
 
     /**
      * Send an {@link android.util.Log#ERROR} log message.
