4b219556e32baac9ecb03b53a7eaeaac4ae2d4f0
christian@voegtle.org <christian@voegtle.org>
2017-07-25 22:09:25 +0200

    Option die Sortierung auszuschalten

--- src/main/java/org/voegtle/weatherwidget/preferences/OrderCriteria.kt
+++ src/main/java/org/voegtle/weatherwidget/preferences/OrderCriteria.kt
@@ -1,20 +1,21 @@
 package org.voegtle.weatherwidget.preferences
 
 enum class OrderCriteria constructor(private val key: String) {
-  location("location"), temperature("temperature"), rain("rain"), humidity("humidity"), default("default");
+  default("default"), location("location"), temperature("temperature"), rain("rain"), humidity("humidity");
 
   companion object {
 
     fun byKey(key: String): OrderCriteria {
-      return values().firstOrNull { it.key == key } ?: location
+      return values().firstOrNull { it.key == key } ?: default
     }
 
     fun byIndex(which: Int): OrderCriteria =
         when (which) {
-          0 -> OrderCriteria.location
-          1 -> OrderCriteria.temperature
-          2 -> OrderCriteria.rain
-          3 -> OrderCriteria.humidity
+          0-> OrderCriteria.default
+          1 -> OrderCriteria.location
+          2 -> OrderCriteria.temperature
+          3 -> OrderCriteria.rain
+          4 -> OrderCriteria.humidity
           else -> OrderCriteria.default
         }
 
