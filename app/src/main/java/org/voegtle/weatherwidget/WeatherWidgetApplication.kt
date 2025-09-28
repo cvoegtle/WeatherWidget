55449dc66ebe438873d388992190e0264fc48f52
christian@voegtle.org <christian@voegtle.org>
2025-09-28 12:06:42 +0200

    Widgets im Hintergrund aktualisieren

--- app/src/main/java/org/voegtle/weatherwidget/WeatherWidgetApplication.kt
+++ app/src/main/java/org/voegtle/weatherwidget/WeatherWidgetApplication.kt
@@ -1,11 +1,35 @@
 package org.voegtle.weatherwidget
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.google.android.material.color.DynamicColors
+import org.voegtle.weatherwidget.widget.WidgetUpdateWorker
+import java.util.concurrent.TimeUnit
 
 class WeatherWidgetApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = androidx.work.Constraints.Builder()
+            .setRequiredNetworkType(androidx.work.NetworkType.CONNECTED)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<WidgetUpdateWorker>(
+            PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MILLISECONDS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            WidgetUpdateWorker::class.java.name,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
