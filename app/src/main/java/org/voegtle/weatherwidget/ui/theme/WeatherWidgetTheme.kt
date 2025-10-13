66f756c7b414ed6f23020e612fb22adbb598b19d
christian@voegtle.org <christian@voegtle.org>
2025-10-13 20:00:43 +0200

    Theme Statusbar lesbar anzeigen

--- app/src/main/java/org/voegtle/weatherwidget/ui/theme/WeatherWidgetTheme.kt
+++ app/src/main/java/org/voegtle/weatherwidget/ui/theme/WeatherWidgetTheme.kt
@@ -1,5 +1,6 @@
 package org.voegtle.weatherwidget.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -7,7 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 import org.voegtle.weatherwidget.preferences.AppTheme
 
 // Erstelle das lightColorScheme mit den Werten aus Color.kt
@@ -59,6 +63,14 @@ fun WeatherWidgetTheme(
         }
         // Fallback auf deine definierten Farben
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
