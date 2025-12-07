72ac7ed23201b88b79bb17a061bcf340bb86bdc9
christian@voegtle.org <christian@voegtle.org>
2025-12-07 18:46:04 +0100

    Timestamp der Daten anzeigen

--- wetterwolkewatch/src/main/java/org/voegtle/wetterwolkewatch/WeatherScreen.kt
+++ wetterwolkewatch/src/main/java/org/voegtle/wetterwolkewatch/WeatherScreen.kt
@@ -2,49 +2,65 @@ package org.voegtle.wetterwolkewatch
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.Text
 import org.voegtle.weatherwidget.data.WeatherData
 import org.voegtle.weatherwidget.location.LocationIdentifier
 import org.voegtle.weatherwidget.location.Position
-import java.util.Date
 import org.voegtle.weatherwidget.util.DataFormatter
+import java.util.Date
 
 @Composable
 fun WeatherScreen(weatherData: WeatherData) {
-    Column(
-        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        val formatter = DataFormatter()
-        Text(
-            text = weatherData.location_short,
-            style = MaterialTheme.typography.displayLarge
-        )
-        Text(
-            text = "${formatter.formatTemperature(weatherData.temperature)} / ${formatter.formatHumidity(weatherData.humidity)}",
-            style = MaterialTheme.typography.bodyLarge
-        )
-        weatherData.rainToday?.let {
-            val rainText = "Regen: ${formatter.formatRain(weatherData.rainToday)}" + if (weatherData.rain != null) formatter.formatRain(weatherData.rain) else ""
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val formatter = DataFormatter()
             Text(
-                text = rainText ,
+                text = weatherData.location_short,
+                style = MaterialTheme.typography.displayLarge
+            )
+            Text(
+                text = "${formatter.formatTemperature(weatherData.temperature)} / ${formatter.formatHumidity(weatherData.humidity)}",
                 style = MaterialTheme.typography.bodyLarge
             )
+            weatherData.rainToday?.let {
+                val rainText = "Regen: ${formatter.formatRain(weatherData.rainToday)}" + if (weatherData.rain != null) " / ${formatter.formatRain(weatherData.rain)}" else ""
+                Text(
+                    text = rainText,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            weatherData.barometer?.let {
+                Text(
+                    text = formatter.formatBarometer(it),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
         }
-        weatherData.barometer?.let {
-            Text(
-                text = formatter.formatBarometer(it),
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
+        Text(
+            text = weatherData.localtime,
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp)
+        )
     }
 }
 
